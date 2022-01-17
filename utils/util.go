b218c6e36143717ed90b15fccac99994064fce53
xwzQmxx <1499273991@qq.com>
2022-01-17 18:09:38 +0800

    modify validated email logic

--- utils/util.go
+++ utils/util.go
@@ -2,12 +2,8 @@ package utils
 
 import "regexp"
 
-var emailRe = regexp.MustCompile(`[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,6}`)
+var emailRe = regexp.MustCompile(`^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,6})$`)
 
-func NormalEmail(email string) string {
-	if v := emailRe.FindStringSubmatch(email); len(v) > 0 {
-		return v[0]
-	}
-
-	return ""
+func IsValidatedEmail(email string) bool {
+	return emailRe.MatchString(email)
 }
