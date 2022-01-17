fb644faa9319600cd46799a3958b8b72d398112c
cz <1499273991@qq.com>
2022-01-17 19:16:24 +0800

    fix review

--- utils/util.go
+++ utils/util.go
@@ -4,6 +4,6 @@ import "regexp"
 
 var emailRe = regexp.MustCompile(`^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,6})$`)
 
-func IsValidatedEmail(email string) bool {
+func IsValidEmail(email string) bool {
 	return emailRe.MatchString(email)
 }
