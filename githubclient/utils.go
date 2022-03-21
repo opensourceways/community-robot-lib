5c5a6cf3b0077d68b57837bf63411a2afc3cdd12
xwzQmxx <1499273991@qq.com>
2022-03-21 20:20:09 +0800

    add utils func for github client

--- githubclient/utils.go
+++ githubclient/utils.go
@@ -3,7 +3,10 @@ package githubclient
 import "github.com/google/go-github/v36/github"
 
 const (
-	ActionOpened = "opened"
+	ActionOpened  = "opened"
+	ActionCreated = "created"
+	ActionReopen  = "reopened"
+	ActionClosed  = "closed"
 )
 
 // GetOrgRepo return the owner and name of the repository
@@ -14,4 +17,9 @@ func GetOrgRepo(repo *github.Repository) (string, string) {
 // IsIssueOpened judge is issue create event
 func IsIssueOpened(action string) bool {
 	return action == ActionOpened
+}
+
+// IsIssueCommentCreated judge is issue comment create event
+func IsIssueCommentCreated(action string) bool {
+	return action == ActionCreated
 }
